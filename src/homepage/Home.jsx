c563fd931e38cfec28545472703b8a911777c304
ygntechnologies <163327903+ygntechnologies@users.noreply.github.com>
2026-04-08 02:14:14 +0530

    Update Home.jsx

--- src/homepage/Home.jsx
+++ src/homepage/Home.jsx
@@ -6,7 +6,6 @@ import { DatePicker } from "antd";
 import { useParams } from "react-router-dom";
 
 const Home = () => {
-  const [base64Image, setBase64Image] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
   const [formData, setFormData] = useState({
     title: "",
@@ -16,6 +15,7 @@ const Home = () => {
     image: "",
     date: "",
   });
+
   const isLoggedIn = localStorage.getItem("isLoggedIn");
   const navigate = useNavigate();
   const { id } = useParams();
@@ -45,35 +45,15 @@ const Home = () => {
     }
   }, [isLoggedIn]);
 
-  const handleImageChange = (event) => {
-    const file = event.target.files[0];
-    const reader = new FileReader();
-
-    reader.onloadend = () => {
-      const base64String = reader.result;
-      setFormData((prevData) => ({
-        ...prevData,
-        image: base64String,
-      }));
-      setBase64Image(base64String);
-    };
-
-    if (file) {
-      reader.readAsDataURL(file);
-    }
-  };
-
   const handleChange = (e) => {
     const { name, value } = e.target;
-    if (name !== "image") {
-      setFormData((prevData) => ({
-        ...prevData,
-        [name]: value,
-      }));
-    }
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
   };
 
-  const handleDateChange = (dateString) => {
+  const handleDateChange = (date, dateString) => {
     setFormData((prevData) => ({
       ...prevData,
       date: dateString,
@@ -83,6 +63,7 @@ const Home = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     setErrorMsg(null);
+
     const options = {
       method: "POST",
       headers: {
@@ -90,7 +71,11 @@ const Home = () => {
       },
       body: JSON.stringify(formData),
     };
-    const url = id ? `${baseURL}/edit-blog/${id}` : `${baseURL}/create-blog`; // Adjust the URL based on the presence of ID
+
+    const url = id
+      ? `${baseURL}/edit-blog/${id}`
+      : `${baseURL}/create-blog`;
+
     fetch(url, options)
       .then((response) => {
         if (!response.ok) {
@@ -108,8 +93,7 @@ const Home = () => {
           image: "",
           date: "",
         });
-        setBase64Image(null);
-        navigate("/blog-list")
+        navigate("/blog-list");
       })
       .catch((error) => {
         setErrorMsg("Something Went Wrong!");
@@ -126,10 +110,7 @@ const Home = () => {
           className="mix-blend-multiply"
           style={{ width: "10%" }}
         />
-        <div
-          onClick={() => navigate("/blog-list")}
-          style={{ cursor: "pointer" }}
-        >
+        <div onClick={() => navigate("/blog-list")} style={{ cursor: "pointer" }}>
           <b>BlogList</b>
         </div>
         <div
@@ -142,135 +123,101 @@ const Home = () => {
           Logout
         </div>
       </div>
+
       <form
         className="shadow-xl max-w-[800px] w-full mx-auto px-[60px] py-[40px] border-[2px] border-gray-400 rounded-md"
         onSubmit={handleSubmit}
       >
-        <h1
-          style={{
-            fontSize: "24px",
-            textAlign: "center",
-            paddingBottom: "40px",
-            fontWeight: "600",
-          }}
-        >
+        <h1 className="text-[24px] text-center pb-[40px] font-semibold">
           Create A Blog
         </h1>
+
         <div className="relative z-0 w-full mb-10 group">
           <input
             type="text"
             name="title"
-            id="title"
-            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            placeholder=" "
+            className="input-style"
             onChange={handleChange}
             required
             value={formData.title}
           />
-          <label
-            for="title"
-            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-          >
-            Title
-          </label>
+          <label className="label-style">Title</label>
         </div>
+
         <div className="relative max-w-sm my-4">
-          <label
-            for="date"
-            className="peer-focus:font-medium absolute text-[18px] text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-0 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-          >
-            Date
-          </label>
+          <label className="label-style text-[18px]">Date</label>
           <DatePicker
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Select date" // Placeholder text
-            onChange={(date, dateString) => handleDateChange(date, dateString)} // Handle date change
+            className="input-box"
+            placeholder="Select date"
+            onChange={handleDateChange}
           />
         </div>
+
         <div className="grid md:grid-cols-2 md:gap-6">
           <div className="relative z-0 w-full mb-7 group">
             <input
               type="text"
               name="name"
-              id="name"
-              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-              placeholder=" "
+              className="input-style"
               onChange={handleChange}
               required
               value={formData.name}
             />
-            <label
-              for="Name"
-              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-            >
-              Name
-            </label>
+            <label className="label-style">Name</label>
           </div>
+
           <div className="relative z-0 w-full mb-7 group">
             <input
               type="text"
               name="type"
-              id="type"
-              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-              placeholder=" "
+              className="input-style"
               onChange={handleChange}
               required
               value={formData.type}
             />
-            <label
-              for="type"
-              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-            >
-              Type
-            </label>
+            <label className="label-style">Type</label>
           </div>
         </div>
-        <div className="flex flex-col mb-10 relative mt-5">
-          <label
-            className="peer-focus:font-medium absolute text-[18px] text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-0 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-            for="image"
-          >
-            Upload file
-          </label>
+
+        <div className="relative z-0 w-full mb-10 group mt-5">
           <input
+            type="text"
             name="image"
-            onChange={handleImageChange}
-            className="block cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-            aria-describedby="image"
-            id="image"
-            type="file"
+            className="input-style"
+            onChange={handleChange}
+            value={formData.image}
           />
+          <label className="label-style">Image URL</label>
         </div>
-        {base64Image && (
+
+        {formData.image && (
           <img
-            src={base64Image}
-            alt="Uploaded"
-            className="max-w-full h-[80px] mt-4 mb-8"
+            src={formData.image}
+            alt="Preview"
+            className="max-w-full h-[80px] mt-[-20px] mb-8"
+            onError={(e) => (e.target.style.display = "none")}
           />
         )}
+
         <div className="relative z-0 w-full mb-5 group">
           <textarea
-            id="description"
             name="description"
             rows="4"
             value={formData.description}
             onChange={handleChange}
-            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            className="input-style"
           ></textarea>
-          <label
-            for="description"
-            className="peer-focus:font-medium absolute text-[18px] text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-0 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-          >
-            Description
-          </label>
+          <label className="label-style text-[18px]">Description</label>
         </div>
+
         <button
           type="submit"
-          className="text-white mt-4 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-8 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          className="text-white mt-4 bg-blue-700 hover:bg-blue-800 rounded-lg text-sm px-8 py-2.5"
         >
           {id ? "Update" : "Submit"}
         </button>
-        {errorMsg && <div className="text-[16px] text-red-600">{errorMsg}</div>}
+
+        {errorMsg && <div className="text-red-600">{errorMsg}</div>}
       </form>
     </>
   );
